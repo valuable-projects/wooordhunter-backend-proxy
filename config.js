9efa3a85492d472df947611df8cd16687152d07c
vviital <vviital@tut.by>
2018-06-24 17:03:26 +0300

    Use correct env variable for travis

--- config.js
+++ config.js
@@ -6,9 +6,7 @@ let mongoDBUrl = env.MONGO_URL || '';
 let mongoDBPort = env.MONGO_PORT || '';
 let mongoDBDatabase = env.MONGO_PORT || '';
 
-console.log('process.env', process.env);
-
-if (process.env === 'TEST') {
+if (process.env.NODE_ENV === 'TEST') {
   mongoDBUrl = mongoDBUrl || 'mongodb://127.0.0.1';
   mongoDBPort = mongoDBPort || 27017;
   mongoDBDatabase = mongoDBDatabase || 'test';
